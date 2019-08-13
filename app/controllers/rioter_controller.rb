bc6bff76daaaea9eb6f5a88ea0dd78c45dea677a
SebastianoPio Matera <spm471@nyu.edu>
2019-08-13 11:40:59 +0200

    Haven't done any redirects for create and destroy

--- app/controllers/rioter_controller.rb
+++ app/controllers/rioter_controller.rb
@@ -2,4 +2,39 @@ class RioterController < ApplicationController
   def index
     @rioters = Rioter.all
   end
+
+  def show
+    @rioter = Rioter.find(params[:id])
+  end
+
+  def new
+    @rioter = Rioter.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @rioter = Rioter.new(rioter_params)
+    @rioter.user = User.find(params[:user_id])
+    @rioter.save
+    #needs a redirection
+  end
+
+  def edit
+    @rioter = Rioter.find(params[:id])
+  end
+
+  def update
+    @rioter = Rioter.find(params[:id])
+    @rioter.update(params[:rioter])
+  end
+
+  def destroy
+    @rioter = Rioter.find(params[:id])
+    @rioter.destroy
+    #needs redirection
+  end
+
+  def rioter_params
+    params.require(:rioter).permit(:name, :availability, :rate, :picture, :description)
+  end
 end
