5fa7e2e117210998b08713d33c869705b0a09b72
George Pearson <pearsongeorgef@gmail.com>
2019-08-14 16:30:35 +0200

    create new rioter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,11 @@
 import "bootstrap";
+import $ from 'jquery';
+import 'select2';
+import 'select2/dist/css/select2.css';
+
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+const initSelect2 = () => {
+  $('.select2').select2();
+};
