e248d33cd34f4feb7792e9db12bf8b6fba764964
George Pearson <pearsongeorgef@gmail.com>
2019-08-16 16:55:00 +0200

    booking fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,10 +11,16 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking_params)
+
+    @booking = Booking.new
+    @booking.location = booking_params[:location]
+    @booking.start = start_date_time
+    @booking.end = end_date_time
     @booking.user = current_user
     @booking.rioter = Rioter.find(params[:rioter_id])
     @booking.save
+
+    redirect_to bookings_path(current_user)
   end
 
   def show
@@ -39,5 +45,25 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start, :end, :location)
+  end
+
+  def start_date_time
+    date_array = date_array(:start)
+    hour_array = hour_array(:start)
+    DateTime.new date_array.first,date_array.second,date_array.third, hour_array.first, hour_array.second
+  end
+
+  def end_date_time
+    date_array = date_array(:end)
+    hour_array = hour_array(:end)
+    DateTime.new date_array.first,date_array.second,date_array.third, hour_array.first, hour_array.second
+  end
+
+  def date_array(date)
+    booking_params[date].split(' ').first.split('-').map(&:to_i)
+  end
+
+  def hour_array(date)
+    booking_params[date].split(' ').second.split(':').map(&:to_i)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import 'select2';
 import 'select2/dist/css/select2.css';
 
-
+import "../plugins/flatpickr"
 
 const initSelect2 = () => {
   $('.select2').select2();
